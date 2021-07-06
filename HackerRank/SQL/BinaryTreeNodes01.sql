0493b5b7a6be825067ef3128e7f713ffea4972a8
yewonim0406 <85602214+yewonim0406@users.noreply.github.com>
2021-07-06 16:12:39 +0900

    Create BinaryTreeNodes01.sql

---
+++ HackerRank/SQL/BinaryTreeNodes01.sql
@@ -0,0 +1,13 @@
+/* Binary Tree Nodes 01 */
+-- 2021.07.06.Tue
+-- https://www.hackerrank.com/challenges/binary-search-tree-1/problem
+-- Advanced Select
+-- Subquery, CASE WHEN, CONCAT
+
+SELECT CASE WHEN p IS NULL THEN CONCAT(n, ' Root')
+            WHEN n IN (SELECT DISTINCT p FROM bst) 
+                 THEN CONCAT(n, ' Inner')
+            ELSE CONCAT(n, ' Leaf')
+        END
+  FROM bst
+ ORDER BY n;
