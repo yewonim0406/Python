aca2faf3416e825f32d7e5c118b7cca4e073cd5a
yewonim0406 <85602214+yewonim0406@users.noreply.github.com>
2021-07-07 16:40:30 +0900

    Update 20210706 Occupations.sql

--- HackerRank/SQL/20210706 Occupations.sql
+++ HackerRank/SQL/20210706 Occupations.sql
@@ -16,3 +16,9 @@ SELECT Doctor, Professor, Singer, Actor
                                      'Singer' AS Singer,
                                      'Actor' AS Actor))
   ORDER BY 1, 2, 3, 4;
+  
+  
+  
+  /* 1. PIVOT을 사용하려면 행의 기준과 열의 기준이 있어야 함
+     2. ROW_NUMBER() 분석함수를 통해서 rn을 생성하지 않으면, PIVOT을 돌렸을때 행의 기준이 없기 때문에 모든 사람이 열거되지 않고, 한 줄만 출력됨
+     3. PIVOT에서 값에 해당하는 expr은 무조건 집계 함수로 표현이 되어야 한다. -> 문자열의 경우 MIN()등을 사용하면 됨 */
