ab2cc931becde164238c166eafc0fbf86dbbdd4d
yewonim0406 <85602214+yewonim0406@users.noreply.github.com>
2021-07-28 14:22:15 +0900

    Update 20210726 ContestLeaderboard.sql

--- HackerRank/SQL/20210726 ContestLeaderboard.sql
+++ HackerRank/SQL/20210726 ContestLeaderboard.sql
@@ -1,23 +1,17 @@
-SELECT h.hacker_id, h.name, SUM(s.score)
+/* Contest Leaderboard */
+
+-- 2021.07.26. Mon
+-- Basic Join
+-- Subquery, 집계 함수
+
+SELECT h.hacker_id, h.name, SUM(s.score) total_score
   FROM hackers h
-  JOIN submissions s
-    ON h.hacker_id = s.hacker_id
- WHERE s.score = (SELECT MAX(score)
-                    FROM submissions
-                   WHERE h.hacker_id = hacker_id
-                     AND s.challenge_id = challenge_id
-                   GROUP BY hacker_id, challenge_id)
- GROUP BY h.hacker_id, h.name                   
-HAVING SUM(s.score) > 0                   
- ORDER BY 3 DESC, 1;  
- 
- 
- SELECT h.hacker_id, h.name, SUM(s.max_score)
-  FROM hackers h
-  JOIN (SELECT hacker_id, MAX(score) max_score
-          FROM submissions
+  JOIN (SELECT hacker_id, MAX(score) score
+          FROM submissions s
          GROUP BY hacker_id, challenge_id) s
     ON h.hacker_id = s.hacker_id
- GROUP BY h.hacker_id, h.name                   
-HAVING SUM(s.max_score) > 0                   
- ORDER BY 3 DESC, 1;  
+ GROUP BY h.hacker_id, h.name
+HAVING SUM(s.score) > 0
+ ORDER BY 3 DESC, 1;
+ 
+ /* 1. 각 챌린지별 가장 높은 점수를 해당 챌린지의 대표 점수로 잡아야 함 */
